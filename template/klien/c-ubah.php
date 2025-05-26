ed6f31e68af2868bf2d1371da4eb67c882195c03
LordVoltech <syamilalghani.9i@gmail.com>
2025-05-26 11:11:26 +0700

    d

--- template/klien/c-ubah.php
+++ template/klien/c-ubah.php
@@ -47,7 +47,7 @@
 if (isset($_POST['proses'])){
     include 'koneksi.php';
   
-    $id = $_POST['id-item'];
+    $id = $_POST['id'];
     $nama = $_POST['nama'];
     $alamat = $_POST['alamat'];
     $nohp = $_POST['nohp'];
